596620f26125a287bb988f4611412ad186ed1d8f
Marlin <50shorty56@gmail.com>
2022-02-02 12:49:55 -0600

    Reverted back to Javascript due to several issues. Updated error catching to produce an error event.

--- apiCom.js
+++ apiCom.js
@@ -1,9 +1,10 @@
+
 /* eslint-disable linebreak-style */
 /* eslint-disable @typescript-eslint/no-var-requires */
+
 const axios = require('axios').default;
 const qs = require('qs');
 const { EventEmitter } = require('events');
-
 
 /**
  * ApiCom
@@ -12,147 +13,138 @@ const { EventEmitter } = require('events');
  * @author - Justin Cardenas
  * @version - 1.0.0
  */
-
 class ApiCom {
-	private _token: string;
-	private _username: string;
-	private _base_url: string;
-	private axios_client: typeof axios;
-	public emitter:typeof EventEmitter;
 	/**
 	 * Builds the api communicator for SpaceTraders
 	 * @param {String} token - The Bearer token of the user
 	 * @param {String} username - Username of the user
 	 */
-	constructor(token:string, username:string) {
+	constructor(token, username) {
 		this._token = token;
 		this._username = username;
 		this._base_url = 'https://api.spacetraders.io';
 		this.axios_client = axios.create({
-			baseURL:this._base_url,
-			headers:{
-				'Authorization':this._token
+			baseURL: this._base_url,
+			headers: {
+				'Authorization': this._token
 			}
 		});
 		this.emitter = new EventEmitter();
 	}
-
 	get getToken() {
 		return this._token;
 	}
 	get getUsername() {
 		return this._username;
 	}
-	set setToken(token:string) {
+	set setToken(token) {
 		this._token = token;
 	}
-	set setUsername(username:string) {
+	set setUsername(username) {
 		this._username = username;
 	}
-
-
 	//Miscellaneous Requests
-
 	/**
 	 * Gets the status of game servers
 	 */
 	async getStatus() {
 		const res = await this.axios_client.request({
-			method:'get',
-			url:`${this._base_url}/game/status`
+			method: 'get',
+			url: `${this._base_url}/game/status`
 		})
-			.catch(err => { throw new Error(err.response.data.error.message); });
-		console.log(`http code: ${ res.status }\nStatus: ${ res.data.status }`);
-
+			.catch(err => { this.emitter.emit('error', new Error(err.response.data.error.message)); return false; });
+		if (!res) {
+			return;
+		}
+		console.log(`http code: ${res.status}\nStatus: ${res.data.status}`);
 	}
 	/**
 	 * Gets the users account
 	 */
 	async getAccount() {
 		const res = await this.axios_client.request({
-			method:'GET',
-			url:'/my/accoun',
+			method: 'GET',
+			url: '/my/account',
 		})
-			.catch(err => { this.emitter.emit('error', new Error(err.response.data.error.message)); });
-		console.log(res);
-	}
+			.catch(err => { this.emitter.emit('error', new Error(err.response.data.error.message)); return false; });
 
-
-
-
-	// System Requests
-
-	/**
-	 * Gets information on a system
-	 * @param symbol - symbol of system
-	 */
-	async getSystemInfo(symbol:string) {
-		const res = await this.axios_client.request({
-			method:'GET',
-			url:`/systems/${symbol.toUpperCase()}`
-		})
-			.catch(err => { throw new Error(err.response.data.error.message); });
-		console.log(res.data);
-	}
-	async getSystemLocations(symbol:string) {
-		const res = await this.axios_client.request({
-			method:'GET',
-			url:`/systems/${symbol}/locations`
-		})
-			.catch(err => { this.emitter.emit('httpError', err); return false;});
-		
 		if (!res) {
 			return;
 		}
 		console.log(res.data);
 	}
-
+	// System Requests
+	/**
+	 * Gets information on a system
+	 * @param symbol - symbol of system
+	 */
+	async getSystemInfo(symbol) {
+		const res = await this.axios_client.request({
+			method: 'GET',
+			url: `/systems/${symbol.toUpperCase()}`
+		})
+			.catch(err => { this.emitter.emit('error',new Error(err.response.data.error.message)); return false; });
+		console.log(res.data);
+	}
+	async getSystemLocations(symbol) {
+		const res = await this.axios_client.request({
+			method: 'GET',
+			url: `/systems/${symbol}/locations`
+		})
+			.catch(err => { this.emitter.emit('error', new Error(err.response.data.error.message)); return false; });
+		if (!res) {
+			return;
+		}
+		console.log(res.data);
+	}
 	// Ship Requests
-
 	/**
 	 * buy a thing
 	 * @param location_symbol - Location of the ship
 	 * @param ship_symbol - symbol of the ship to buy
 	 */
-	async buyShip(location_symbol:string, ship_symbol:string) {
+	async buyShip(location_symbol, ship_symbol) {
 		const res = await axios({
-			method:'POST',
-			url:'/my/ships',
-			params:{
-				location:location_symbol,
-				type:ship_symbol
+			method: 'POST',
+			url: '/my/ships',
+			params: {
+				location: location_symbol,
+				type: ship_symbol
 			},
 			paramsSerializer: params => {
 				return qs.stringify(params);
 			}
 		})
-			.catch(err => { throw new Error(err.response.data.error.message); });
+			.catch(err => { this.emitter.emit('error', new Error(err.response.data.error.message)); return false; });
+		if (!res) {
+			return;
+		}
 		console.log(res.data);
 	}
-
-
 	/**
 	 * Gets the ship listings of the system
 	 * @param symbol - Symbol of system
 	 * @param ship_class - Class of ships (defaults to null)
 	 * @returns - http request
 	 */
-	async getSystemShipListings(symbol:string, ship_class:string) {
+	async getSystemShipListings(symbol, ship_class) {
 		if (typeof ship_class === 'undefined') {
 			ship_class = null;
 		}
 		const res = await axios({
-			method:'get',
-			url:`/systems/${symbol.toUpperCase()}/ship-listings`,
-			params:{
-				'class':ship_class
+			method: 'get',
+			url: `/systems/${symbol.toUpperCase()}/ship-listings`,
+			params: {
+				'class': ship_class
 			},
 			paramsSerializer: params => {
 				return qs.stringify(params);
-
 			}
 		})
-			.catch(err => { throw new Error(err.response.data.error.message); });
+			.catch(err => { this.emitter.emit('error', new Error(err.response.data.error.message)); return false; });
+		if (!res) {
+			return;
+		}
 		console.log(res.data);
 	}
 	/**
@@ -160,8 +152,8 @@ class ApiCom {
 	 */
 	async getUserShips() {
 		const res = await axios({
-			method:'get',
-			url:'/my/ships',
+			method: 'get',
+			url: '/my/ships',
 		});
 		console.log(res.data);
 	}
@@ -170,23 +162,22 @@ class ApiCom {
 	 * @param symbol - symbol of the ship to look for
 	 * @param system - system to look in
 	 */
-	async getBuyLocation(symbol:string, system:string) {
+	async getBuyLocation(symbol, system) {
 		const res = await axios({
 			method: 'get',
-			url:`/systems/${system}/ship-listings`,
+			url: `/systems/${system}/ship-listings`,
 		})
-			.catch(err => { this.emitter.emit('httpError', err); return false; });
-		
+			.catch(err => { this.emitter.emit('error', new Error(err.response.data.error.message)); return false; });
 		if (!res) {
 			return;
 		}
+
 		let i = 0;
 		for (const listing of res.data.shipListings) {
 			if (listing.type === symbol) {
 				i++;
 				for (const location of listing.purchaseLocations) {
 					console.log(location);
-					
 				}
 				break;
 			}
@@ -194,41 +185,44 @@ class ApiCom {
 		if (i === 0) {
 			this.emitter.emit('error', new Error('This ship does not exist'));
 		}
-	
-		
 	}
 	/**
 	 * Gets all available ships
 	 * @param ship_class - Sort by class (optional)
 	 */
-	async getAvailableShips(ship_class:string) {
+	async getAvailableShips(ship_class) {
 		if (typeof ship_class === 'undefined') {
 			ship_class = null;
 		}
 		const res = await axios({
-			method:'get',
-			url:'/types/ships',
-			params:{
-				'class':ship_class
+			method: 'get',
+			url: '/types/ships',
+			params: {
+				'class': ship_class
 			},
 			paramsSerializer: params => {
 				return qs.stringify(params);
 			}
 		})
-			.catch(err => { throw new Error(err.response.data.error.message); });
-
+			.catch(err => { this.emitter.emit('error', new Error(err.response.data.error.message)); return false; });
+		if (!res) {
+			return;
+		}
 		console.log(res.data);
 	}
 	/**
 	 * Gets information on given ship
-	 * @param shipID - actual id of the ship 
+	 * @param shipID - actual id of the ship
 	 */
-	async getShipInfo(shipID:string) {
+	async getShipInfo(shipID) {
 		const res = await this.axios_client.request({
-			method:'GET',
-			url:`/my/ships/${shipID}`
+			method: 'GET',
+			url: `/my/ships/${shipID}`
 		})
-			.catch(err => { throw new Error(err.response.data.error.message); });
+			.catch(err => { this.emitter.emit('error', new Error(err.response.data.error.message)); return false; });
+		if (!res) {
+			return;
+		}
 		console.log(res.data);
 	}
 	/**
@@ -236,25 +230,23 @@ class ApiCom {
 	 */
 	async getShips() {
 		const res = await this.axios_client.request({
-			method:'GET',
-			url:'/my/ships'
+			method: 'GET',
+			url: '/my/ships'
 		})
-			.catch(err => { throw new Error(err.response.data.error.message); });
+			.catch(err => { this.emitter.emit('error', new Error(err.response.data.error.message)); return false; });
+		if (!res) {
+			return;
+		}
 		console.log(res.data);
 	}
-
-
-
 	// Loans Requests
-
-
 	/**
 	 * get loans you have taken out
 	 */
 	async getUserLoans() {
 		const res = await this.axios_client.request({
-			method:'GET',
-			url:'/my/loans'
+			method: 'GET',
+			url: '/my/loans'
 		})
 			.catch(err => { this.emitter.emit('httpError', err); return false; });
 		if (!res) {
@@ -262,25 +254,22 @@ class ApiCom {
 		}
 		console.log(res.data);
 	}
-
-
 	/**
 	 * Take a loan
 	 * @param type - Type of loan to take
 	 */
-	async takeLoan(type:string) {
+	async takeLoan(type) {
 		const res = await this.axios_client.request({
-			method:'POST',
-			url:'/my/loans',
-			params:{
-				type:type.toUpperCase()
+			method: 'POST',
+			url: '/my/loans',
+			params: {
+				type: type.toUpperCase()
 			},
 			paramsSerializer: params => {
 				return qs.stringify(params);
 			}
 		})
-			.catch(err => { this.emitter.emit('httpError', err); return false;});
-		
+			.catch(err => { this.emitter.emit('httpError', err); return false; });
 		if (!res) {
 			return;
 		}
@@ -291,8 +280,8 @@ class ApiCom {
 	 */
 	async getLoans() {
 		const res = await this.axios_client.request({
-			method:'GET',
-			url:'/types/loans'
+			method: 'GET',
+			url: '/types/loans'
 		})
 			.catch(err => { this.emitter.emit('httpError', err); return false; });
 		if (!res) {
@@ -300,27 +289,25 @@ class ApiCom {
 		}
 		console.log(res.data);
 	}
-
 	/**
-	 * 
+	 *
 	 * @param loanID - id of the loan (NOT TYPE)
 	 */
-	async payLoan(loanID:string) {
+	async payLoan(loanID) {
 		const res = await this.axios_client.request({
-			method:'PUT',
-			url:`/my/loans/${loanID}`,
+			method: 'PUT',
+			url: `/my/loans/${loanID}`,
 		})
 			.catch(err => { this.emitter.emit('httpError', err); return false; });
-		
 		if (!res) {
 			return;
 		}
 		console.log(res.data);
 	}
-	
-	
 }
 
 module.exports = {
 	ApiCom
 };
+
+
---
+++ index.js
@@ -0,0 +1,25 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const token = 'Bearer 32f01f32-ec1e-4be5-8c7d-1e8e0cc56da1';
+const username = 'SadLonelyGamer';
+const { ApiCom } = require('./apiCom');
+const com = new ApiCom(token, username);
+com.getStatus();
+// com.getSystemShipListings('OE', 'MK-I').catch(err => { console.log(err.message); });
+// com.getBuyLocation('EM-MK-I', 'OE').catch(err => { console.log(err.message); });
+// com.buyShip('OE-PM-TR', 'EM-MK-I').catch(err => { console.log(err.message); });
+// com.getUserShips().catch(err => { console.log(err.message); });
+com.getAccount();
+// com.getAvailableShips('MK-I').catch(err => { console.log(err.message); });
+// com.getAccount().catch(err => { console.log(err.message); });
+// com.getSystemInfo('OE').catch(err => { console.log(err.message); });
+// com.getShipInfo('ckyngh01b27970415s6jsksbsze').catch(err => { console.warn(err.message); });
+// com.getShips().catch(err => { console.warn(err.message); });
+// com.getUserLoans().catch(err => { console.warn(err.message); });
+// com.getLoans().catch(err => { console.log(err.message); });
+// com.takeLoan('STARTUP').catch(err => { console.log(err.message); });
+// com.payLoan('ckydjoxd4102674115s6l5skc2l4').catch(err => console.log(err.message));
+// com.getSystemLocations('Os');
+com.emitter.on('error', (err) => {
+	console.log(err.message);
+});
